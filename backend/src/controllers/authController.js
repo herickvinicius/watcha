17dc6271d606d55cff82c846a42f9b243b11cabd
herickvinicius <herick.gustavo@gmail.com>
2020-11-18 18:15:59 -0400

    Adicionado o gerador de token para usuarios que fizerem a conta e para os que fizerem login (/register e /authenticate). Duraçao padrao do token e de 24 horas.

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const authConfig = require('../config/auth.json')
 const Profile = require('../models/profile')
 const User = require('../models/user')
 const router = express.Router()
 
+function generateToken(params = {}) {
+    return jwt.sign({ params }, authConfig.secret, {
+        expiresIn: 86400
+    })
+}
+
+// REGISTRAR USER
 router.post('/register', async (req, res) => {
     const { email } = req.body
 
@@ -21,29 +31,41 @@ router.post('/register', async (req, res) => {
         profile.userId = user._id
         profile.save()
 
+        // Apaga o .password do user pra não ser retornado.
         user.password = undefined
 
-        return res.send({ user })
+        return res.send({
+            user,
+            toke: generateToken({ id: user.id })
+        })
     } catch (err) {
         console.log(err)
         return res.status(400).send({ error: 'Registration failed' })
     }
 })
 
+// AUTENTICAR USER
 router.post('/authenticate', async (req, res) => {
     const { email, password } = req.body
 
+    // Procura no banco por um usuário com o mesmo email.
     const user = await User.findOne({ email }).select('+password')
 
+    // Se não encontrar, retorna erro.
     if(!user)
         return res.status(400).send({ error: 'User not found' })
     
+    // Se a senha fornecida pelo user não for válida, retorna erro.
     if(!await bcrypt.compare(password, user.password))
         return res.status(400).send({ error: 'Invalid password' })
 
+    // Apaga o .password do user pra não ser retornado.
     user.password = undefined
     
-    res.send({ user })
+    res.send({
+        user,
+        token : generateToken({ id: user.id })
+    })
 })
 
 module.exports = app => app.use('/auth', router)
