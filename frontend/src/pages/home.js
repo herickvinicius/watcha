2244cede1258569fbbbe03dea2c5df670a9b0f50
herickvinicius <herick.gustavo@gmail.com>
2020-11-20 10:25:11 -0400

    FIX: Redireciona para /login se o usuário não está autenticado.

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { CssBaseline, Grid, Typography } from "@material-ui/core";
 import APIService from "../utils/apiService";
+import { useHistory } from "react-router";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token === null) {
+      console.log(token);
+      history.push("/login");
+    }
+  });
 
   useEffect(() => {
     APIService.fetchPopular()
@@ -11,7 +21,7 @@ const Home = () => {
       .catch((e) => console.error(e));
   }, []);
 
-  console.log(movies);
+  //console.log(movies);
 
   return (
     <>
